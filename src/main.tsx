f8d70dac30455fb64d574d085ef44d663c34838c
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-30 04:23:21 +0900

    feat: global style 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
+import GlobalStyle from "./globalStyle.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
+);
