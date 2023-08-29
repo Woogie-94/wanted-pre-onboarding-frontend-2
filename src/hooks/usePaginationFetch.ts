106158634774efed06ffc093f6b68bfd498bf97f
Woogie-94 <dmsdmr9411@gmail.com>
2023-08-30 04:18:32 +0900

    feat: isLoading 추가

--- src/hooks/usePaginationFetch.ts
+++ src/hooks/usePaginationFetch.ts
@@ -9,29 +9,32 @@ interface Params<T> {
 
 const usePaginationFetch = <T>({ initialData, fetchFn }: Params<T>) => {
   const [data, setData] = useState<T[]>(initialData);
+  const [isLoading, setIsLoading] = useState(false);
   const pagination = useMemo(() => new Pagination(), []);
 
   const getPagination = useCallback(
     (result: PaginationFetchResult<T[]>) => {
       pagination.setNextPage = result.nextPage;
       setData(prev => [...prev, ...result.data]);
+      setIsLoading(false);
     },
     [pagination],
   );
 
   const fetch = useCallback(() => {
-    return fetchFn().then(getPagination);
+    setIsLoading(true);
+    fetchFn().then(getPagination);
   }, [fetchFn, getPagination]);
 
   const fetchNextPage = useCallback(() => {
     if (pagination.isLast) {
       return;
     }
-
-    return fetchFn(pagination.getNextPage).then(getPagination);
+    setIsLoading(true);
+    fetchFn(pagination.getNextPage).then(getPagination);
   }, [fetchFn, pagination, getPagination]);
 
-  return { data, fetch, fetchNextPage };
+  return { data, fetch, fetchNextPage, isLoading };
 };
 
 export default usePaginationFetch;
